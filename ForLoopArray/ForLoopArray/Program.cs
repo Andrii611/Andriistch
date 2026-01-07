033b4f49b558488fe8bb008db7c736930a0b43d3
Opilane TTHK <ffgggfegujd@gmail.com>
2026-01-07 14:04:34 +0200

    pepe shneine

---
+++ ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] array = { "H", "e", "l", "l", "o" };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
