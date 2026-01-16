37c46afbebcac29f2f6a526e36d4a24bd9da56b1
Opilane TTHK <opilane@tthk.ee>
2026-01-16 15:05:21 +0200

    txt

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FailWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta faili läbi konsooli");
+
+            string filePath = @"C:/Users/opilane/Desktop/pechenki.txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filePath, inputText);
+        }
+    }
+}
